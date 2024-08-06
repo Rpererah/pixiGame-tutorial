0475cadbf00d93c84f491db860426bde13e3e63a
rpererah <rpererah@gmail.com>
2024-08-06 19:03:57 -0300

    fix hitbox

--- src/Flappy/utils/test.js
+++ src/Flappy/utils/test.js
@@ -2,6 +2,7 @@ import { Graphics } from "pixi.js";
 
 export function desenharHitbox(container, objeto, cor = 0xff0000, alpha = 0.5) {
   const hitbox = new Graphics();
+
   atualizarHitbox(hitbox, objeto, cor, alpha);
   container.addChild(hitbox);
   return hitbox;
@@ -10,7 +11,6 @@ export function desenharHitbox(container, objeto, cor = 0xff0000, alpha = 0.5) {
 export function atualizarHitbox(hitbox, objeto, cor = 0xff0000, alpha = 0.5) {
   hitbox.clear();
   const bounds = objeto.getBounds();
-  hitbox.beginFill(cor, alpha);
-  hitbox.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
-  hitbox.endFill();
+  hitbox.rect(bounds.x, bounds.y, bounds.width, bounds.height);
+  hitbox.fill({ color: cor, alpha });
 }
